5bea1a1ae39390131e9373a3bf66b9c7706e2ad5
Saumya <ssbajracharya@outlook.com>
2026-02-22 10:10:15 -0600

    Add project files.

---
+++ Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A_6_3_CallQueue
+{
+    internal class Node<T>
+    {
+
+        public T Data;
+        public Node<T>? Next;
+
+        // Constructor
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace A_6_3_CallQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
